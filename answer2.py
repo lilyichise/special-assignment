bcf8d3066099b8bde595462da6a314646a5ec21b
lilyichise <95495611+lilyichise@users.noreply.github.com>
2023-04-24 01:33:24 -0400

    done

--- answer2.py
+++ answer2.py
@@ -1,4 +1,10 @@
 import pandas as pd
+# pandas documentation: https://pandas.pydata.org/docs/user_guide/index.html#user-guide
+# other pandas info: https://pandas.pydata.org/pandas-docs/stable/getting_started/intro_tutorials/03_subset_data.html
+# index - https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.index.html?highlight=index#pandas.DataFrame.index
+# splitting objects into groups - https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#splitting-an-object-into-groups
+# selecting a subset of rows from a group - https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.core.groupby.DataFrameGroupBy.head.html?highlight=head
+
 
 # read CSV file into pandas dataframe
 df = pd.read_csv('col.csv')
