7008b349906e4ff6e42e6a4812897ad3f05b8280
lilyichise <95495611+lilyichise@users.noreply.github.com>
2023-04-24 01:11:27 -0400

    progress

---
+++ answer2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# read CSV file into pandas dataframe
+df = pd.read_csv('col.csv')
+
+# initially set Result to 'No' for all rows
+df['Result'] = 'No'
+
+# Group the dataframe by Col1 using 'df.groupby()',
+# and use .head(1) to select the first row of each group.
+# Use.loc[] to set Result to 'Yes' for the selected rows
+df.loc[df.groupby('Col1').head(1).index, 'Result'] = 'Yes'
